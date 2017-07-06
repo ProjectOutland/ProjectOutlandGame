64c7d194bf50130a1e27ed7eb719add3b58e0152
SAE\jirishdonges <1005809@student.sae.edu.au>
2017-07-06 17:45:50 +1000

    Plants now spawn when music is going Stops spawning when music is stopped Spawns on the ground and upright instead of upside down in the air

---
+++ New Unity Project 1/Assets/scripts/PlantDeath.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantDeath : MonoBehaviour {
+
+    public float Life = 3;
+
+        // Update is called once per frame
+        void Update () {
+
+        Life -= Time.deltaTime;
+
+        if (Life <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
