4ac2601a2a282696fb115f73fda4eb1054815229
SAE\zcroslow <adequatesilver@gmail.com>
2017-07-12 13:23:36 +1000

    Early version of Main Menu - Zac

---
+++ New Unity Project 1/Assets/mainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class mainMenu : MonoBehaviour
+{
+	public string levelToLoad = "ProjectOutland";
+	public void Play ()
+	{
+		SceneManager.LoadScene (levelToLoad);
+		Debug.Log ("You Are Pressing Play");
+	}
+
+	public void Quit () 
+	{
+		Debug.Log ("You Are Pressing Quit");
+		Application.Quit ();
+	}
+}
+
