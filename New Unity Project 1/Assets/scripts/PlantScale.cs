12d7a12826000d0901cadf5e49d8dd6d9da5d52e
SAE\jwhittaker4 <1007359@student.sae.edu.au>
2017-07-17 11:32:39 +1000

    New trees added, new script to randomly change tree size, please add to tree array.

---
+++ New Unity Project 1/Assets/scripts/PlantScale.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantScale : MonoBehaviour {
+
+    public float ThisScale;
+    public float min;
+    public float max;
+
+	// Use this for initialization
+	void Start () {
+        ThisScale = Random.Range(min, max);
+        transform.localScale += new Vector3(ThisScale, ThisScale, ThisScale);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
